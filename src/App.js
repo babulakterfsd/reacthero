d935216b9079b27060885c9034a46ac1b9d12322
Babul Akter <babulakterfsd@gmail.com>
2021-06-15 02:28:27 +0600

    tailwind initialized

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div class="container mx-auto">
+           <h1 className="text-gray-700 p-8">Hi there, I am tailwind !</h1>
+        </div>
+      </>
+    )
+  }
+}
+
+export default App;
